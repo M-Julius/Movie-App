7de898762389853b0ea0dcd557a898f80eb347d7
M-Julius <mjulius.dev@gmail.com>
2024-08-05 15:28:43 +0700

    refactor error boundary

--- app/screens/ErrorScreen/ErrorBoundary.tsx
+++ app/screens/ErrorScreen/ErrorBoundary.tsx
@@ -1,5 +1,5 @@
 import React, { Component, ErrorInfo, ReactNode } from "react"
-import { ErrorComponent } from "./error-component"
+import { ErrorDetails } from "./ErrorDetails"
 
 interface Props {
   children: ReactNode
@@ -16,16 +16,20 @@ interface State {
  * app. It follows the "error boundary" pattern in React. We're using a
  * class component because according to the documentation, only class
  * components can be error boundaries.
- *
- * Read more here:
- *
- * @link: https://reactjs.org/docs/error-boundaries.html
+ * @see [Documentation and Examples]{@link https://docs.infinite.red/ignite-cli/concept/Error-Boundary/}
+ * @see [React Error Boundaries]{@link https://react.dev/reference/react/Component#catching-rendering-errors-with-an-error-boundary}
+ * @param {Props} props - The props for the `ErrorBoundary` component.
+ * @returns {JSX.Element} The rendered `ErrorBoundary` component.
  */
 export class ErrorBoundary extends Component<Props, State> {
   state = { error: null, errorInfo: null }
 
   // If an error in a child is encountered, this will run
   componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    // Only set errors if enabled
+    if (!this.isEnabled()) {
+      return
+    }
     // Catch errors in any components below and re-render with error message
     this.setState({
       error,
@@ -33,8 +37,8 @@ export class ErrorBoundary extends Component<Props, State> {
     })
 
     // You can also log error messages to an error reporting service here
-    // This is a great place to put BugSnag, Sentry, Honeybadger, etc:
-    // reportErrorToCrashReportingService(error)
+    // This is a great place to put BugSnag, Sentry, crashlytics, etc:
+    // reportCrash(error)
   }
 
   // Reset the error back to null
@@ -43,8 +47,8 @@ export class ErrorBoundary extends Component<Props, State> {
   }
 
   // To avoid unnecessary re-renders
-  shouldComponentUpdate(nextProps: Readonly<any>, nextState: Readonly<any>): boolean {
-    return nextState.error !== nextProps.error
+  shouldComponentUpdate(nextProps: Readonly<Props>, nextState: Readonly<State>): boolean {
+    return nextState.error !== this.state.error
   }
 
   // Only enable if we're catching errors in the right environment
@@ -59,7 +63,7 @@ export class ErrorBoundary extends Component<Props, State> {
   // Render an error UI if there's an error; otherwise, render children
   render() {
     return this.isEnabled() && this.state.error ? (
-      <ErrorComponent
+      <ErrorDetails
         onReset={this.resetError}
         error={this.state.error}
         errorInfo={this.state.errorInfo}
